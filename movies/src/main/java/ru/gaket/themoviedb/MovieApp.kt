ae6020ce6c312e9abf7869cef2f171a8acbcd4ba
Artur Badretdinov <artursletter@gmail.com>
2021-10-18 15:41:22 -0400

    Add DI feature

--- movies/src/main/java/ru/gaket/themoviedb/MovieApp.kt
+++ movies/src/main/java/ru/gaket/themoviedb/MovieApp.kt
@@ -5,5 +5,7 @@ import ru.gaket.themoviedb.di.AppComponent
 
 class MovieApp : Application() {
 
+  // You can use whatever DI framework you are used to
+  // We have some Toothpic bindings in base classes that do the injection for us
   val appComponent: AppComponent by lazy { AppComponent(this) }
 }
