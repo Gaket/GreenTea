7cfc38e5d2275022a19a36bafc87bd481dd3dae5
Artur Badretdinov <artursletter@gmail.com>
2021-12-18 20:23:40 -0500

    Add Timber and logging Trees

---
+++ tools/src/main/java/net/gaket/tools/CrashlyticsTree.kt
@@ -0,0 +1,27 @@
+package net.gaket.tools
+
+import com.google.firebase.crashlytics.FirebaseCrashlytics
+import timber.log.Timber
+
+
+/**
+ * Tree that is responsible for sending crashes and logs to Crashlytics
+ */
+class CrashlyticsTree : RemoteLoggerTree() {
+
+  private val crashlytics: FirebaseCrashlytics? =
+    try {
+      FirebaseCrashlytics.getInstance()
+    } catch (throwable: Throwable) {
+      Timber.e(throwable)
+      null
+    }
+
+  override fun logException(t: Throwable) {
+    crashlytics?.recordException(t)
+  }
+
+  override fun logTraceRecord(taggedMessage: String) {
+    crashlytics?.log(taggedMessage)
+  }
+}
---
+++ tools/src/main/java/net/gaket/tools/WtfNotifyQaTree.kt
@@ -0,0 +1,26 @@
+package net.gaket.tools
+
+import android.app.Application
+import android.os.Handler
+import android.os.Looper
+import android.util.Log
+import android.widget.Toast
+import timber.log.Timber
+
+
+/**
+ * A tree that alerts users in non-release builds about an Assert-level log via a long Toast message
+ */
+class WtfNotifyQaTree(private val context: Application) : Timber.Tree() {
+
+  override fun log(priority: Int, tag: String?, message: String, t: Throwable?) {
+    if (priority != Log.ASSERT) {
+      return
+    }
+
+    Handler(Looper.getMainLooper()).post {
+      Toast.makeText(context, "Internal Error! $message", Toast.LENGTH_SHORT).show()
+      Toast.makeText(context, "Internal Error! $message", Toast.LENGTH_LONG).show()
+    }
+  }
+}
