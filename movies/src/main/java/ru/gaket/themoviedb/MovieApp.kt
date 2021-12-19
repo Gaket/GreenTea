d0f1862357ca1c70c6c8c816299f268ada02e177
Artur Badretdinov <artursletter@gmail.com>
2021-12-18 20:24:26 -0500

    Plant Timber Forest in the App class

--- movies/src/main/java/ru/gaket/themoviedb/MovieApp.kt
+++ movies/src/main/java/ru/gaket/themoviedb/MovieApp.kt
@@ -1,11 +1,27 @@
 package ru.gaket.themoviedb
 
 import android.app.Application
+import net.gaket.tools.CrashlyticsTree
+import net.gaket.tools.WtfNotifyQaTree
 import ru.gaket.themoviedb.di.AppComponent
+import ru.gaket.themoviedb.utils.FullstoryTree
+import timber.log.Timber
 
 class MovieApp : Application() {
 
   // You can use whatever DI framework you are used to
   // We have some Toothpic bindings in base classes that do the injection for us
   val appComponent: AppComponent by lazy { AppComponent(this) }
+
+  override fun onCreate() {
+    super.onCreate()
+    plantTimberForest()
+  }
+
+  private fun plantTimberForest() {
+    Timber.plant(Timber.DebugTree())
+    Timber.plant(CrashlyticsTree())
+    Timber.plant(WtfNotifyQaTree(this))
+    Timber.plant(FullstoryTree())
+  }
 }
